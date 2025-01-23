a6f8d3a86a2471d98b5325e26e9dca78fb29813c
Avashneupane9857 <juisch20220011252@gmail.com>
2025-01-23 16:53:17 +0530

    :lipstick: hero

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -2,35 +2,32 @@ import { design, flower } from "../assets";
 
 function Hero() {
   return (
-    <header className=" w-[88%] mx-auto relative top-38 flex justify-between">
-      <div className="text-[#EDF738] relative top-32">
-        act <span>---</span> together
+    <div className="relative top-14 ">
+      <div className="flex flex-col items-center">
+        {" "}
+        <img className="w-14" src={flower} alt="" />
       </div>
-      <div className="text-[#EDF738] flex flex-col items-center ">
-        <div className="">
-          {" "}
-          <img
-            src={flower}
-            alt=""
-            className="absolute -top-20 left-1/2 -translate-x-4 h-8 w-8 "
-          />
-        </div>
 
-        <div>
+      <header className=" w-[88%] mx-auto relative top-18 flex justify-between">
+        {" "}
+        <div className="text-[#EDF738] relative top-32">
+          act <span>---</span> together
+        </div>
+        <div className="text-[#EDF738]   ">
           <img src={design} alt="" />
           <div className="text-[80px] text-center -top-1.5 font-bold leading-24 tracking-widest absolute left-0 w-full h-full">
             <h1>REVEAL</h1>
             <h1>THE BE[A]ST</h1>
-            <button className="bg-[#EDF738] relative  text-white h-6 tracking-normal rounded-sm w-20 text-[10px] ">
-              HYPEREALIST
-            </button>
           </div>
+          <button className="bg-[#EDF738] relative left-54 -top-5  text-black h-6 tracking-normal rounded-sm w-20 text-[10px] ">
+            HYPEREALIST
+          </button>
         </div>
-      </div>
-      <div className="text-[#EDF738] relative top-32">
-        create <span className="">---</span> your story
-      </div>
-    </header>
+        <div className="text-[#EDF738] relative top-32">
+          create <span className="">---</span> your story
+        </div>
+      </header>
+    </div>
   );
 }
 
