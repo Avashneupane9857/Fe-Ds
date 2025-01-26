3c4249b0eb82073f9fe6c5034f7c8ec83832708a
Avashneupane9857 <juisch20220011252@gmail.com>
2025-01-26 17:36:31 +0530

    Refactored

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,38 +1,36 @@
 import { design, flower } from "../assets";
-
 function Hero() {
   return (
-    <div className="relative top-10 md:top-14 px-4">
+    <div className="relative top-14 ">
       <div className="flex animate-spin flex-col items-center">
-        <img className="w-10 md:w-14" src={flower} alt="Flower" />
+        {" "}
+        <img className="w-14 " src={flower} alt="" />
       </div>
-      <div className="max-w-7xl mx-auto relative md:top-18 flex flex-col md:flex-row justify-between items-center">
-        <div className="text-[#EDF738] text-center md:text-left relative md:top-32 mb-4 md:mb-0">
-          act <span className="hidden md:inline">---</span> together
+      <header className=" w-[88%] mx-auto relative top-18 flex justify-between">
+        {" "}
+        <div className="text-[#EDF738] text relative top-32">
+          act <span>---</span> together
         </div>
-
-        <div className="text-[#EDF738] text-center relative">
-          <img src={design} className="mx-auto mb-4 md:mb-0" alt="Design" />
-          <div className="text-3xl md:text-[80px] font-bold tracking-widest">
+        <div className="text-[#EDF738]   ">
+          <img src={design} className="Design" alt="" />
+          <div className="text-[80px]  text-center -top-1.5 font-bold leading-24 tracking-widest absolute left-0 w-full h-full">
             <h1>REVEAL</h1>
             <h1>THE BE[A]ST</h1>
           </div>
-          <button className="bg-[#EDF738] text-black mt-2 md:mt-0 md:absolute md:left-54 md:-top-5 h-8 px-4 rounded-sm text-xs tracking-normal">
+          <button className="bg-[#EDF738] relative left-54 -top-5  text-black h-6 tracking-normal rounded-sm w-20 text-[10px] ">
             HYPEREALIST
           </button>
         </div>
-
-        <div className="text-[#EDF738] text-center md:text-right relative md:top-32 mt-4 md:mt-0">
-          create <span className="hidden md:inline">---</span> your story
+        <div className="text-[#EDF738] text2 relative top-32">
+          create <span className="">---</span> your story
         </div>
-      </div>
-
-      <div className="bg-white absolute right-0 top-48 md:top-65 w-16 md:w-10 p-2 h-24 md:h-36 hidden md:block">
-        <h1 className="font-bold text-xl md:text-2xl pb-4">W.</h1>
+      </header>
+      <div className="bg-white absolute whiteBox right-0 top-65  w-10 p-2   h-36">
+        {" "}
+        <h1 className="font-bold text-2xl pb-6 ">W.</h1>
         <h1 className="transform rotate-90">Honors</h1>
       </div>
     </div>
   );
 }
-
 export default Hero;
