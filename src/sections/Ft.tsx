036219f1e5add599c806b58884d7f01b909ca7df
Avashneupane9857 <juisch20220011252@gmail.com>
2025-01-26 17:24:11 +0530

    responsiveness

--- src/sections/Ft.tsx
+++ src/sections/Ft.tsx
@@ -2,26 +2,32 @@ import { Be, fb, insta, ln, pt, spo } from "../assets";
 
 function Ft() {
   return (
-    <div className="w-[80%] h-auto flex flex-col gap-18  bg-black  text-white  mx-auto ">
-      <div className="flex justify-between bg-black font-light text-[12px] w-[70%] mx-auto">
-        <h1>Politique de confidentialité</h1>
-        <h1>Politique des cookies</h1>
-        <h1>Termes et conditions</h1>
-        <h1>© 2022 Hyperealist</h1>
+    <div className="w-full max-w-7xl mx-auto px-4 bg-black text-white py-8">
+      <div className="flex flex-col md:flex-row justify-between text-center md:text-left space-y-6 md:space-y-0">
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 text-xs font-light">
+          <h1>Politique de confidentialité</h1>
+          <h1>Politique des cookies</h1>
+          <h1>Termes et conditions</h1>
+          <h1>© 2022 Hyperealist</h1>
+        </div>
+
+        <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-8 text-xl font-bold">
+          <h1>Projects</h1>
+          <h1>Concept</h1>
+          <h1>Playlist</h1>
+          <h1>Contact</h1>
+        </div>
       </div>
-      <div className="flex  bg-black justify-between text-xl font-bold w-[80%] mx-auto">
-        <h1>Projects</h1>
-        <h1>Concept</h1>
-        <h1>Playlist</h1>
-        <h1>Contact</h1>
-      </div>
-      <div className="flex bg-black justify-between  font-bold ">
-        <img src={insta} alt="" />
-        <img src={fb} alt="" />
-        <img src={ln} alt="" />
-        <img src={Be} alt="" />
-        <img src={spo} alt="" />
-        <img src={pt} alt="" />
+
+      <div className="flex justify-center md:justify-between items-center mt-8 space-x-4 md:space-x-0">
+        {[insta, fb, ln, Be, spo, pt].map((icon, index) => (
+          <img
+            key={index}
+            src={icon}
+            alt={`Social icon ${index + 1}`}
+            className="w-6 md:w-auto"
+          />
+        ))}
       </div>
     </div>
   );
